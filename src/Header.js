314c58ab2e52ef44bb35b03c099700565dc0d600
Melanie Panem <16231664+melanierosson@users.noreply.github.com>
2021-06-08 06:53:30 -0700

    following Clever Programmer tutorial a little bit each day

---
+++ src/Header.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './Header.css';
+import { Link } from 'react-router-dom';
+import SearchIcon from '@material-ui/icons/Search';
+
+function Header() {
+    return (
+        <nav className="header">
+            <Link to="/login">
+                <img 
+                className="header__logo" 
+                src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+                alt="Amazon Top Logo"
+            /> 
+            </Link>
+            <div className="header__search">
+                <input type="text" className="header__searchInput" />
+                <SearchIcon className="header__searchIcon" />
+            </div>
+            <div className="header__nav">
+
+                <Link to="/login" className="header__link">
+                    <div className="header__option">
+                        <span>Hello You</span>
+                    </div>
+                </Link>
+
+            </div>
+        </nav>
+    )
+}
+
+export default Header
