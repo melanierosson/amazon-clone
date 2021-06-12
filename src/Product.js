95463f915dd04f9f9ffb6473d7038288e9bb7bda
Melanie Panem <16231664+melanierosson@users.noreply.github.com>
2021-06-12 12:08:01 -0700

    adding more actions to the data layer

--- src/Product.js
+++ src/Product.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import "./Product.css";
+import { useStateValue } from './StateProvider';
 
 function Product({ id, title, image, price, rating, alt }) {
+
+    const [{ basket }, dispatch] = useStateValue();
+
+    const addToBasket = () => {
+        dispatch({
+            type: 'ADD_TO_BASKET',
+            item: {
+                id: id,
+                title: title,
+                image: image,
+                price: price,
+                rating: rating
+            },
+        });
+    };
+
     return (
         <div className="product">
             <div class="product__info">
@@ -23,7 +40,7 @@ function Product({ id, title, image, price, rating, alt }) {
                 src={image}
                 alt={alt}
             />
-            <button>Add To Basket</button>
+            <button onClick={addToBasket}>Add To Basket</button>
         </div>
     );
 }
--- src/StateProvider.js
+++ src/StateProvider.js
@@ -7,7 +7,11 @@ export const StateContext = createContext();
 
 // Build a provider
 export const StateProvider = ({ reducer, initialState, children }) => {
-    <StateContext.Provider value={useReducer(reducer, initialState)}>
-        {children}
-    </StateContext.Provider>
-}
+    return (
+        <StateContext.Provider value={useReducer(reducer, initialState)}>
+            {children}
+        </StateContext.Provider>
+    )
+};
+
+export const useStateValue = () => useContext(StateContext);
