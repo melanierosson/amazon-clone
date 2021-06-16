2c01652e9526fd3d63f4af1dbbc2dcf9b6cc1e1c
Melanie Panem <16231664+melanierosson@users.noreply.github.com>
2021-06-16 16:06:31 -0700

    wow done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Header from './Header';
 import Home from './Home';
 import Checkout from './Checkout';
 import Login from './Login';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { useStateValue } from './StateProvider';
+import { auth } from "./firebase";
 
 function App() {
+  const [{ user }, dispatch] = useStateValue();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        // user logged in
+        dispatch({
+          type: "SET_USER",
+          user: authUser
+        })
+      } else {
+        // user logged out
+        dispatch({
+          type: "SET_USER",
+          user: null
+        })
+      }
+    });
+
+    return () => {
+      unsubscribe();
+    }
+  }, [])
+
   return (
     <Router>
       <div className="app">
